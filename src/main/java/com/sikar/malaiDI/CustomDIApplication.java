7869ee452386d85643561313a5bcd27a66bbd7ff
Elumalai Ramalingam <mynameiselumalai@gmail.com>
2024-03-10 19:19:31 -0400

    basic structure

---
+++ src/main/java/com/sikar/malaiDI/CustomDIApplication.java
@@ -0,0 +1,13 @@
+package com.sikar.malaiDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomDIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomDIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sikar/malaiDI/CustomDIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikar.malaiDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
